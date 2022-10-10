1aba95a2130a3fa1f89f0431b9bc3617ba875ac3
Tony Rockdriguez <ars.proy@gmail.com>
2022-10-09 23:40:05 -0500

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-//Código en C por Antonio Rodriguez
-//Fecha: 09-10-2022
-
+/*
+* Código en C por Antonio Rodriguez
+* Fecha: 09-10-2022
+* Ejercicio de git basico
+*/
 int main(void){
 	printf("Hola mundo!!");
 	return 0;
